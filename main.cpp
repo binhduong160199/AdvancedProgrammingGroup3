c8cd5bfd83a3e9b7ff78f23f0b45709f6f8bd5da
MY PC <muhammadfarasat@yahoo.com>
2025-09-27 16:48:29 +0200

    removed extra code.

--- main.cpp
+++ main.cpp
@@ -2,21 +2,9 @@
 #include "MiddleSquare.h"
 #include "Benchmark.h"
 #include <iostream>
-using namespace std;
 
 int main() {
-    Xorshift xs(2463534242u);
-    MiddleSquare ms(6759);
-
-    cout << "Xorshift: ";
-    for (int i = 0; i < 10; i++) cout << xs.next() << " ";
-    cout << "\n";
-
-    cout << "Middle Square: ";
-    for (int i = 0; i < 10; i++) cout << ms.next() << " ";
-    cout << "\n\n";
-
-    cout << "=== Benchmark (1 million iterations) ===\n";
+    std::cout << "=== Benchmark (1 million iterations) ===" << std::endl;
     Xorshift xs2(2463534242u);
     MiddleSquare ms2(6759);
 
